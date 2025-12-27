30ef939472aba8806e4f1a5b5b86893803c71ad9
Divi202 <divya8217274@gmail.com>
2025-12-27 17:25:43 +0530

    add: custom hover effect on navbar items & CTA buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,18 +5,27 @@ const Hero = () => {
     <div id="hero" className="h-full my-24 flex gap-10">
       <section className="flex flex-col gap-5 justify-center w-[70%]">
         <h1 className="text-5xl font-bold">
+          {/* Heading */}
           Hello, It's <span className="text-rose-400 font-bold">Divya</span>
         </h1>
+        {/* Sub-heading */}
         <h2 className="text-3xl">
           I'm a{" "}
           <span className="text-rose-400 font-bold">Software Developer</span>{" "}
         </h2>
+        {/* Discription */}
         <p className="text-slate-200 text-base">
           I'm a passionate problem solver skilled in Javascript, C++, DSA, and
           full-stack development, driven to build impactful AI-based and
           web-based solutions. Lorem ipsum dolor sit amet, consectetur
           adipisicing elit.
         </p>
+        {/* Social links */}
+        <div className="flex gap-4 mt-4">
+          <Button name="Resume" />
+          <Button name="Contact" />
+        </div>
+        {/* CTA button */}
         <div className="flex gap-4 mt-4">
           <Button name="Resume" />
           <Button name="Contact" />
