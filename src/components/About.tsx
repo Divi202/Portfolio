307ad3824c21ab578fd9edf530833bc8c091a504
Divi202 <divya8217274@gmail.com>
2025-12-28 16:23:56 +0530

    feat: about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,14 @@
+import { Button } from "./ui/button";
+
 const About = () => {
   return (
     <section id="about" className="h-full pb-32 flex flex-col gap-16">
       {/* Section Heading  */}
-      <h2 className="text-xl font-semibold text-center">About Me</h2>
+      <h2 className="text-2xl font-semibold text-center text-rose-400">
+        About Me
+      </h2>
       {/* Section Content  */}
-      <div className="h-full flex gap-10">
+      <div className="h-full flex gap-20">
         {" "}
         {/* Left content : Image  */}
         <div className="flex items-center justify-center w-[40%]">
@@ -15,19 +19,27 @@ const About = () => {
           />
         </div>
         {/* Right content : Text  */}
-        <div className="flex flex-col items-start w-[60%] gap-2">
+        <div className="flex flex-col items-start justify-center w-[60%] gap-2">
           {/* - Content heading: Developement/Building is my Passion */}
-          <h3 className="ml-10 text-lg font-medium">Building is My Passion</h3>
+          <h3 className="text-lg font-medium text-rose-400">
+            Development is My Passion
+          </h3>
           {/* Description about me  */}
-          <p className="ml-10 text-base text-justify">
+          <p className="text-base text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
             provident veniam laudantium dolorem omnis laboriosam excepturi
             nostrum labore, nemo ratione debitis odit repellat, quam
             reprehenderit doloribus earum iste voluptas possimus nesciunt fuga
             qui libero! Illum perferendis vero sapiente. Doloribus sint
             molestias facere sapiente veniam neque qui cumque unde eius
-            repellendus.
+            repellendus. Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Sapiente corrupti facilis, veritatis quasi numquam sequi iste
+            delectus deserunt esse est aspernatur, excepturi modi corporis
+            commodi officia adipisci! Vitae, ex nam?
           </p>
+          <div className="mt-4">
+            <Button name="Resume" />
+          </div>
         </div>
       </div>
     </section>
--- src/components/ui/social-buttons.tsx
+++ src/components/ui/social-buttons.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { IconBrandLeetcode } from "@tabler/icons-react";
 import { HoverBorderGradient } from "./hover-border-gradient";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export function SocialButton(props) {
   return (
@@ -9,7 +11,16 @@ export function SocialButton(props) {
         as="button"
         className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 text-sm font-semibold"
       >
-        <span>{props.iconName}</span>
+        <span>
+          {/* {props.iconName == "Leetcode" ? (
+            <IconBrandLeetcode />
+          ) : props.iconName == "Github" ? (
+            <FaGithub />
+          ) : (
+            <FaLinkedin />
+          )} */}
+          <IconBrandLeetcode />
+        </span>
       </HoverBorderGradient>
     </div>
   );
