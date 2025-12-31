7edd185896a92440bd9e4dcacca35e41b86b0060
Divi202 <divya8217274@gmail.com>
2025-12-31 17:15:47 +0530

    feat: add experiece section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,58 @@
 import SectionHeading from "./section-heading";
+import { Timeline } from "./ui/timeline";
 const Experience = () => {
+  const data = [
+    {
+      title: "2025",
+      content: (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">QA Tester</h2>
+          <p className="text-md text-rose-400">Keywords Studios</p>
+          <ul className="list-disc pl-5 mt-2 space-y-2 mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+            <li className="pb-2">
+              Worked on AAA game titles ensuring gameplay testing, bug tracking,
+              and performance validation across platforms
+            </li>{" "}
+            <li className="pb-2">
+              Collaborated with devs and designers to enhance user experience
+            </li>
+            <li className="pb-2">Logged 100+ critical bugs using JIRA</li>
+            <li>Contributed to day-zero patch readiness</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "2024",
+      content: (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">Master of Computer Applications</h2>
+          <p className="text-md text-rose-400">
+            Delhi Skill and Entrepreneurship University
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "2022",
+      content: (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">
+            Bachelor of Computer Application
+          </h2>
+          <p className="text-md text-rose-400">
+            Guru Gobind Singh Indraprastha University
+          </p>
+        </div>
+      ),
+    },
+  ];
   return (
     <section id="experience" className="h-full pb-32 flex flex-col gap-16">
       {/* Section Heading  */}
-      <SectionHeading headingName="Experience" />
+      <SectionHeading headingName="Experience & Education" />
       {/* Section Content  */}
-      <div>Section content</div>
+      <Timeline data={data} />
     </section>
   );
 };
