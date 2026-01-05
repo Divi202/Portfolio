a40148cf24d672d65bf644b6fe8b90396b5be741
Divi202 <divya8217274@gmail.com>
2026-01-05 16:53:08 +0530

    feat: add animation on skill section

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,6 +1,6 @@
 const SkillCard = (props) => {
   return (
-    <div className="border flex flex-col px-12 py-6 justify-center items-center rounded-md">
+    <div className="border flex flex-col px-12 py-6 justify-center items-center rounded-md shrink-0">
       <img className="h-15 w-15" src={props.logo} alt={props.name} />
       <p className="mt-4">{props.name}</p>
     </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,24 +1,40 @@
 import SkillCard from "./SkillCard";
 import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
 const Skills = () => {
+  const skills = [
+    { logo: "src/assets/skills/c.svg", name: "C++" },
+    { logo: "src/assets/skills/html.svg", name: "HTML" },
+    { logo: "src/assets/skills/css.svg", name: "CSS" },
+    { logo: "src/assets/skills/javascript.svg", name: "JavaScript" },
+    { logo: "src/assets/skills/react.svg", name: "React" },
+    { logo: "src/assets/skills/typescript.svg", name: "TypeScript" },
+    { logo: "src/assets/skills/git.svg", name: "Git" },
+    { logo: "src/assets/skills/tailwindcss.svg", name: "Tailwind CSS" },
+    { logo: "src/assets/skills/vite.svg", name: "Vite" },
+  ];
+
   return (
-    <section id="skills" className="h-full py-24 flex flex-col gap-16">
+    <section id="skills" className="h-full py-24 flex flex-col gap-16 ">
       {/* Section Heading  */}
       <SectionHeading headingName="Skills" />
       {/* Section Content  */}
-      <div className="flex gap-4 border-2 px-10 py-20 flex-wrap justify-center rounded-md">
-        <SkillCard logo="src/assets/skills/c.svg" name="C++" />
-        <SkillCard logo="src/assets/skills/html.svg" name="HTML" />
-        <SkillCard logo="src/assets/skills/css.svg" name="CSS" />
-        <SkillCard logo="src/assets/skills/javascript.svg" name="JavaScript" />
-        <SkillCard logo="src/assets/skills/react.svg" name="React" />
-        <SkillCard logo="src/assets/skills/typescript.svg" name="TypeScript" />
-        <SkillCard logo="src/assets/skills/git.svg" name="Git" />
-        <SkillCard
-          logo="src/assets/skills/tailwindcss.svg"
-          name="Tailwind CSS"
-        />
-        <SkillCard logo="src/assets/skills/vite.svg" name="Vite" />
+      <div className="flex gap-4 border-2 px-10 py-20 justify-center overflow-hidden rounded-md ">
+        <div className="overflow-hidden w-full">
+          <motion.div
+            className="flex gap-6 w-max"
+            animate={{ x: ["0%", "-50%"] }}
+            transition={{
+              repeat: Infinity,
+              duration: 15, // ⬅ smoother
+              ease: "linear",
+            }}
+          >
+            {[...skills, ...skills].map((item, i) => (
+              <SkillCard key={i} logo={item.logo} name={item.name} />
+            ))}
+          </motion.div>
+        </div>
       </div>
     </section>
   );
